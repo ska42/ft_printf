81e63a4edde0555968e203ec44107e6e421c9319
Leo MARTIN <lmartin@e1r2p2.42.fr>
2019-10-23 11:00:20 +0200

    debut putdouble

---
+++ libft/ft_putdouble_fd.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putdouble_fd.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmartin <lmartin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/23 10:49:23 by lmartin           #+#    #+#             */
+/*   Updated: 2019/10/23 11:00:03 by lmartin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		after_coma()
+{
+
+}
+
+int		before_coma()
+{
+	
+}
+
+int		ft_putdouble_fd(double n, int fd)
+{
+	int					ret;
+	unsigned long		value;
+	int					size;
+	int					before_coma;
+	int					after_coma;
+
+
+	value = n;
+	while ()
+
+}
