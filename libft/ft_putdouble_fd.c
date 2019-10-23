6969a16e9cba49d535ae408593632c69cfe0e5f4
Leo MARTIN <lmartin@e1r2p2.42.fr>
2019-10-23 11:08:41 +0200

    g faim

--- libft/ft_putdouble_fd.c
+++ libft/ft_putdouble_fd.c
@@ -6,32 +6,32 @@
 /*   By: lmartin <lmartin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 10:49:23 by lmartin           #+#    #+#             */
-/*   Updated: 2019/10/23 11:00:03 by lmartin          ###   ########.fr       */
+/*   Updated: 2019/10/23 11:08:27 by lmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		after_coma()
+int		after_coma(unsigned long nb, int fd)
 {
 
 }
 
-int		before_coma()
+int		before_coma(unsigned long nb, int fd)
 {
-	
+
 }
 
 int		ft_putdouble_fd(double n, int fd)
 {
-	int					ret;
-	unsigned long		value;
-	int					size;
-	int					before_coma;
-	int					after_coma;
+	int								ret;
+	unsigned long double			value;
+	unsigned long					before_coma;
+	unsigned long					after_coma;
 
-
+	if (n < 0)
+		ft_putchar_fd('-', fd);
 	value = n;
-	while ()
+	before_coma = value;
 
 }
